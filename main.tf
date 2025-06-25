5e583ba65c516f881206a6c8a290a6c7adc2c187
Orealyz <reguememartin@gmail.com>
2025-06-25 12:02:22 +0200

    test

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_mysql_flexible_server" "mysql" {
-  name                   = "mysql-${var.environment}"
+  name                   = "mysql-${var.environment}-2025-martin"
   resource_group_name    = azurerm_resource_group.rg.name
   location               = azurerm_resource_group.rg.location
   administrator_login    = "mysqladmin"
